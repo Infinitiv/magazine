1c7f8e85e6acd876ea94e1474fe290a7c66a35e7
Vladimir Markovnin <markovnin@gmail.com>
2013-10-23 12:37:15 +0400

    Изменение имени проекта в продакшн окружении

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-Rating::Application.configure do
+Magazine::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
