acf22b6bd6254aadf9b8eefb1399066a3cda191d
Vladimir Markovnin <markovnin@gmail.com>
2013-10-23 12:45:20 +0400

    seed - группы пользователей

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,6 @@
 ArticleType.create(name: 'articles')
 ArticleType.create(name: 'anounses')
 ArticleType.create(name: 'news')
+Group.create(name: 'administrators', administrator: true, editor: true, viewer: true)
+Group.create(name: 'editors', administrator: false, editor: true, viewer: true)
+Group.create(name: 'viewers', administrator: false, editor: false, viewer: true)
