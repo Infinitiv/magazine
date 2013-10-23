13e46de72160659e530fb33dfe4125b8d86220ce
Vladimir Markovnin <markovnin@gmail.com>
2013-10-23 12:42:43 +0400

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+ArticleType.create(name: 'articles')
+ArticleType.create(name: 'anounses')
+ArticleType.create(name: 'news')
